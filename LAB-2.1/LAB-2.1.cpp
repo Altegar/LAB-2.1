350604b031d8748eb7ed6bc1029118732a7b8ee0
Sushomeister <91095463+Sushomeister@users.noreply.github.com>
2021-12-16 14:10:33 +0200

    Добавьте файлы проекта.

---
+++ LAB-2.1/LAB-2.1.cpp
@@ -0,0 +1,28 @@
+﻿// LAB-2.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// LAB_02.cpp
+// Сушинський Ігор
+// Лабораторна робота №2.1
+// Лінійні програми.
+// Варіант 18
+
+#include <iostream>;
+#include <cmath>;
+
+using namespace std;
+
+int main()
+{
+    double a, z1, z2;
+
+    cout << "a = "; cin >> a;
+
+    z1 = (((a + 2) / (sqrt(2 * a))) - ((a) / (sqrt(2 * a) + 2)) + ((2) / (a - sqrt(2 * a))) * (sqrt(a) - sqrt(a)) / (a + 2));
+    z2 = 1 / (sqrt(a) + sqrt(2));
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
